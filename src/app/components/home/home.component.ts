a5cdc7f92e61d938b6deac0c1f41ac01376e0a5d
Tomislav Blazeka <Chogos@users.noreply.github.com>
2025-03-14 17:28:39 +0100

    List patients in a pagianted table instad of analyses

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,22 +1,49 @@
-import { Component, OnInit } from '@angular/core';
-import { AnalysisService } from '../../services/analysis.service';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { PatientService, Patient } from '../../services/patient.service';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, RouterLink],
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterLink,
+    MatTableModule,
+    MatPaginatorModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-  analyses: any[] = [];
+export class HomeComponent implements AfterViewInit {
+  displayedColumns: string[] = ['oib', 'firstName', 'lastName', 'dateOfBirth', 'email'];
+  dataSource = new MatTableDataSource<Patient>([]);
 
-  constructor(private analysisService: AnalysisService) {}
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  ngOnInit(): void {
-    this.analysisService.getAnalyses().subscribe(data => {
-      this.analyses = data;
+  constructor(private patientService: PatientService) {}
+
+  ngOnInit() {
+    this.patientService.getPatients().subscribe((patients) => {
+      this.dataSource.data = patients;
     });
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 }
