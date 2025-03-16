563d8d4d6ac44fb8921ec45fa5ac72f01052d6e3
Tomislav Blazeka <Chogos@users.noreply.github.com>
2025-03-16 15:10:37 +0100

    Go home on app name click and add back/forward navigation

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -2,13 +2,22 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-toolbar',
-  imports: [RouterLink, MatToolbarModule, MatButtonModule],
+  imports: [RouterLink, MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.css'
 })
 export class ToolbarComponent {
+  constructor() {}
 
+  goBack(): void {
+    window.history.back();
+  }
+
+  goForward(): void {
+    window.history.forward();
+  }
 }
