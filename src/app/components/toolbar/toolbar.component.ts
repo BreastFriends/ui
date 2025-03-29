f5c3cbbc4b1f975bc4babd1de9cdb5967059f661
Tomislav Blazeka <Chogos@users.noreply.github.com>
2025-03-29 02:47:06 +0100

    Add login

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -3,15 +3,18 @@ import { RouterLink } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
-  imports: [RouterLink, MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [RouterLink, MatToolbarModule, MatButtonModule, MatIconModule, CommonModule],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss'
 })
 export class ToolbarComponent {
-  constructor() {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   goBack(): void {
     window.history.back();
@@ -19,5 +22,18 @@ export class ToolbarComponent {
 
   goForward(): void {
     window.history.forward();
+  }
+
+  getLoggedInUser(): string | null {
+    return this.authService.getLoggedInUser();
+  }
+
+  isLoggedIn(): boolean {
+    return this.authService.isLoggedIn();
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,20 +5,27 @@ import { Injectable } from '@angular/core';
 })
 export class AuthService {
   private isAuthenticated = false;
+  private loggedInUser: string | null = null;
 
   login(username: string, password: string): boolean {
-    if (username === 'doctor' && password === 'doctor') {
+    if (username === 'doc' && password === 'doc') {
       this.isAuthenticated = true;
+      this.loggedInUser = 'Dr. Ana Anić';
       return true;
     }
     return false;
   }
 
-  logout() {
+  logout(): void {
     this.isAuthenticated = false;
+    this.loggedInUser = null;
   }
 
   isLoggedIn(): boolean {
     return this.isAuthenticated;
+  }
+
+  getLoggedInUser(): string | null {
+    return this.loggedInUser;
   }
 }
